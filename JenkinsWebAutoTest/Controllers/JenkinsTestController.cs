7c4893e4e38c87dcf840dc17a646fde405157f53
BurNer <zxc37880@gmail.com>
2018-09-14 16:44:22 +0800

    test2

---
+++ JenkinsWebAutoTest/Controllers/JenkinsTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JenkinsWebAutoTest.Controllers
+{
+    public class JenkinsTestController : Controller
+    {
+        // GET: JenkinsTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
